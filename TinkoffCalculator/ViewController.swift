27af146ca7664dae5c8fb5ab6c5372e243ef2349
Gastronom <terlimbombom0@gmail.com>
2024-02-15 18:44:26 +0300

    All buttons are laidout and call function print title of each

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.titleLabel?.text else { return }
+        print(buttonText)
+        
+    }
+    //    @IBAction func buttonPressed(_ sender: UIButton) {
+//        guard let buttonText = sender.titleLabel?.text else { return }
+//        
+//        print(buttonText)
+//    }
 }
 
